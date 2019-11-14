0f76b2f1ba20dbd89de6585dd78828745d0651f0
anastephane <56960955+anastephane@users.noreply.github.com>
2019-11-14 07:38:19 -0300

    Integração do eclipse com o GitHub

---
+++ ifsol/src/main/java/projeto/ifsol/IfsolApplication.java
@@ -0,0 +1,13 @@
+package projeto.ifsol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfsolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfsolApplication.class, args);
+	}
+
+}
